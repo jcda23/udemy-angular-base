cdd726deab994aad542912bac7452932767743f5
jcda23 <jcda_23@hotmail.com>
2023-02-27 20:38:37 -0500

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './contador/heroes/heroe/heroe.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
 
 @NgModule({
-  declarations: [AppComponent, ContadorComponent, HeroeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HeroesModule, ContadorModule],
   providers: [],
   bootstrap: [AppComponent],
 })
